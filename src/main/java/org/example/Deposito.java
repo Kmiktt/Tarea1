3a8bb39a2fecebbd5aa6bf52b54822a9d1585522
Kmiktt <kmilagato05@gmail.com>
2025-04-22 13:13:54 -0400

    arreglo de deposito generico

--- src/main/java/org/example/Deposito.java
+++ src/main/java/org/example/Deposito.java
@@ -2,14 +2,14 @@ package org.example;
 import java.util.ArrayList;
 
 public class Deposito<T> {
-    private ArrayList<Bebida> al;
+    private ArrayList<T> al;
     public  Deposito(){
         al = new ArrayList<T>();
     }
     public void add(Bebida b){
         al.add(b);
     }
-    public Bebida get(){
+    public T get(){
         if ((al.size()!=0)) return al.remove(0);
         else return null;
     }
