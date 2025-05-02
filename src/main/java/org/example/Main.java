fddd30a507525438df06ea196c7553e8066c4a65
Kmiktt <kmilagato05@gmail.com>
2025-05-02 13:58:03 -0400

    pruebas de compra de productos mas su vuelto

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<Moneda> money = new ArrayList<Moneda>();
         /*
-        el siguiente for verifica
+        el siguiente for verifica:
         i=0: moneda inexistente
         i=1: moneda insuficiente
         i=2: producto fuera de rango
@@ -16,10 +16,10 @@ public class Main {
         money.add(new Moneda1000());
         money.add(new Moneda1000());
         Expendedor exp = new Expendedor(1);
-        
+
         for (int i = 0; i < 4; i++) {
             try {
-                Comprador comp = new Comprador(money.removeFirst(), i + 4, exp);
+                Comprador comp = new Comprador(money.removeFirst(), i + 3, exp);
             } catch (NoHayProductoException e) {
                 System.out.println("El producto no se encuentra o no existe");
             } catch (PagoIncorrectoException e) {
@@ -30,13 +30,27 @@ public class Main {
         }
         //aqui prueba sacar un producto que no queda mas
         try {
-            Comprador comp = new Comprador(new Moneda1000(), 4, exp);
+            Comprador comp = new Comprador(new Moneda1000(), 5, exp);
         } catch (NoHayProductoException e) {
             System.out.println("El producto no se encuentra o no existe");
         } catch (PagoIncorrectoException e) {
             System.out.println("La moneda ingresada no existe");
         } catch (PagoInsuficienteException e) {
             System.out.println("El valor del producto es mayor a la cantidad de dinero ingresada");
+        }
+        //y aqui compra 1 de cada producto y muestra el vuelto recibido acorde a cada uno
+        Expendedor exp2 = new Expendedor(1);
+        for (int i = 1; i < 6; i++) {
+            try {
+                Comprador comp = new Comprador(new Moneda1000(), i, exp2);
+                System.out.println("Compro "+comp.queConsumiste()+" y de vuelto tuvo "+comp.cuantoVuelto());
+            } catch (NoHayProductoException e) {
+                System.out.println("El producto no se encuentra o no existe");
+            } catch (PagoIncorrectoException e) {
+                System.out.println("La moneda ingresada no existe");
+            } catch (PagoInsuficienteException e) {
+                System.out.println("El valor del producto es mayor a la cantidad de dinero ingresada");
+            }
         }
 
     }
